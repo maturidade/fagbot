648a39e870c77e3e1df2938252030c9a70e03dbc
Rodrigo Navarro <rnavarro1@gmail.com>
2015-05-21 14:33:02 -0300

    Using Conduit API to download memes.

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ if (process.env.NODE_ENV != "production") require("dotenv").load({ silent: true 
 
 var APP_URL = process.env.APP_URL;
 var BOT_TOKEN = process.env.BOT_TOKEN;
-var PHABRICATOR_DB_URL = process.env.PHABRICATOR_DB_URL;
-var PHABRICATOR_S3_BUCKET = process.env.PHABRICATOR_S3_BUCKET;
-var AWS_KEY_ID = process.env.AWS_KEY_ID;
-var AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 var SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
 var CONDUIT_USER = process.env.CONDUIT_USER;
 var CONDUIT_CERTIFICATE = process.env.CONDUIT_CERTIFICATE;
@@ -26,21 +22,19 @@ var PhabricatorMemeRepository = require("./meme/phabricator/repository");
 var PhabricatorMemeDownloader = require("./meme/phabricator/downloader");
 var FallbackMemeRepository = require("./meme/fallback_repository");
 
+var MemeRepository = require("./meme/phabricator/repository");
 var VoteRepository = require("./vote/repository");
 var Bot = require("./slack/bot");
 var Conduit = require("./phabricator/conduit");
-var PhabricatorNotificator = require("./phabricator/notificator");
 
+var conduit = new Conduit(CONDUIT_USER, CONDUIT_CERTIFICATE, CONDUIT_API_URL);
+var phabricatorNotificator = new PhabricatorNotificator(conduit);
 var localMemes = new LocalMemeRepository(path.resolve(__dirname, "memes"));
-var phabricatorMemeDownloader = new PhabricatorMemeDownloader(PHABRICATOR_S3_BUCKET, AWS_KEY_ID, AWS_SECRET_ACCESS_KEY);
-var phabricatorMemes = new PhabricatorMemeRepository(PHABRICATOR_DB_URL, phabricatorMemeDownloader);
+var phabricatorMemes = new MemeRepository(conduit);
 var memeRepository = new FallbackMemeRepository(localMemes, phabricatorMemes);
 
 var voteRepository = new VoteRepository();
 var bot = new Bot(BOT_TOKEN);
-
-var conduit = new Conduit(CONDUIT_USER, CONDUIT_CERTIFICATE, CONDUIT_API_URL);
-var phabricatorNotificator = new PhabricatorNotificator(conduit);
 
 // Pool upvote
 // ex: +sauce
@@ -101,7 +95,7 @@ app.get("/cowsay", (req, res) => {
       f: options[1],
       text: options[2]
     };
-  } 
+  }
   request({
     uri: SLACK_WEBHOOK_URL,
     method: "POST",
